008b6ae70c588bff3a42a7f44fdc233f247b0aaf
Mark Ronquillo <markronquillo23@gmail.com>
2016-12-10 23:48:40 +0800

    Finish Cookies and Session Chapter

--- rango/urls.py
+++ rango/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page$',
         views.add_page, name="add_page"),
     url(r'^about/$', views.about, name="about"),
+
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
